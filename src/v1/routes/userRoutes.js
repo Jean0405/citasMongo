aa75a262782ec41b6003023acca82bb218e963c7
Jean0405 <jeanpierrea0405@gmail.com>
2023-08-21 23:02:14 -0500

    feat: :construction: Se inicia desarrollo de la ruta para USUARIO
    Esta ruta permitira realizar peticiones GET, POST y demás a la colección USERS

---
+++ src/v1/routes/userRoutes.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import * as userController from "../../controllers/userControllers.js";
+import { validateSchema } from "../../helpers/validateSchema.js";
+import { userSchema } from "../../middlewares/userMiddleware.js";
+
+const v1User = Router();
+
+v1User
+  .get("/", userController.getAllUsers)
+  .get("/:userId", userController.getUser)
+  .post("/", validateSchema(userSchema), userController.postUser);
+
+// const xd = {
+//   "ID": 1005328638,
+//   "names": {
+//     "name": "Deiby",
+//     "surname": "Peñato",
+//   },
+//   "phone": "3133705455",
+//   "address": "Calle 31 #2-39",
+//   "email": "deiby.penaa@gmail.com",
+//   "genre": {
+//     "nameGenre": "masculino",
+//     "abbreviation": "M",
+//   },
+//   "attendant": {
+//     "ID": 1122334455,
+//     "name": "Brandom Peñato",
+//     "phone": "3224097916",
+//     "address": "Calle 31 #2-39",
+//   },
+// };
+export default v1User;
