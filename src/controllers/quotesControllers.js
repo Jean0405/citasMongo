527bf7968fe5cb6d961f685d81c3f27769335468
Jean0405 <jeanpierrea0405@gmail.com>
2023-08-22 20:49:27 -0500

    feat: :construction: Se mantiene en desarrollo las rutas de QUOTES
    Se agregaron nuevos módulos de peticiones HTTP al servidor, además, estas rutas aun siguen en contrucción debido a que un faltan peticiones a realizar.

--- src/controllers/quotesControllers.js
+++ src/controllers/quotesControllers.js
@@ -1,15 +1,43 @@
-
+import * as quotesServices from "../services/quoteServices.js";
 
 export const getAllQuotes = async (req, res) => {
   try {
-    let data = await quoteServices.getAllQuotes();
+    let data = await quotesServices.getAllQuotes();
     res
       .status(200)
       .send({ status: 200, message: "CORRECTLY OBTAINED DATA", data });
   } catch (error) {
     res.status(500).send({
       status: 500,
-      errorInfo: { message: "ERROR IN OBTAINING DATA", error: error },
+      errorInfo: { message: "ERROR IN OBTAINING DATA", error: error.message },
+    });
+  }
+};
+
+export const getAllQuotesInOrder = async (req, res) => {
+  try {
+    let data = await quotesServices.getAllQuotesInOrder(req.params.order);
+    res
+      .status(200)
+      .send({ status: 200, message: "CORRECTLY OBTAINED DATA", data });
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      errorInfo: { message: "ERROR IN OBTAINING DATA", error: error.message },
+    });
+  }
+};
+
+export const getQuotesByGenre = async (req, res) => {
+  try {
+    let data = await quotesServices.getQuotesByGenre(req.params.genre);
+    res
+      .status(200)
+      .send({ status: 200, message: "CORRECTLY OBTAINED DATA", data });
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      errorInfo: { message: "ERROR IN OBTAINING DATA", error: error.message },
     });
   }
 };
--- src/v1/routes/quotesRoutes.js
+++ src/v1/routes/quotesRoutes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import * as quotesController from "../../controllers/quotesControllers.js";
 
 const v1Quotes = Router();
 
-v1Quotes.get("/", quotesController.getAllQuotes);
+v1Quotes
+  .get("/", quotesController.getAllQuotes)
+  .get("/order=:order", quotesController.getAllQuotesInOrder)
+  .get("/genre=:genre", quotesController.getQuotesByGenre);
 
 export default v1Quotes;
